cf50d6403ebfb47d73d133dfc8dd2b89f956864b
chandut027 <chandut027@gmail.com>
2017-11-13 16:47:06 +0100

    adding Main class to read the file and convert

---
+++ src/main/java/com/tnt/ExcelReading.java
@@ -0,0 +1,104 @@
+package com.tnt;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class ExcelReading {
+
+    public static void echoAsCSV(Sheet sheet, BufferedWriter bw, String lang) throws IOException {
+        Row row;
+        int count = 1;
+        for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+            row = sheet.getRow(i);
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+                bw.write("    <trans-unit id=\""+count+"\">");
+                bw.newLine();
+                bw.write("       <source xml:lang=\"en\"><![CDATA[" + row.getCell(j++) + "]]></source>");
+                bw.newLine();
+                bw.write("       <target xml:lang=\"" + lang + "\"><![CDATA[" + row.getCell(j) + "]]></target>");
+                bw.newLine();
+                bw.write("    </trans-unit>");
+                bw.newLine();
+                count++;
+            }
+        }
+    }
+
+    /**
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String fileName = "en_gb-track.xls";
+        String lang = getLocale(fileName);
+        InputStream inp = null;
+        try {
+            File fout = new File("/Users/chandu/Documents/poc/cq-jsp-scripts/excel-to-xliff/en_gb.dict.xliff");
+            FileOutputStream fos = new FileOutputStream(fout);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+            inp = new FileInputStream("/Users/chandu/Documents/poc/cq-jsp-scripts/excel-to-xliff/en_gb-track.xls");
+            Workbook wb = WorkbookFactory.create(inp);
+            bw.write("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
+            bw.newLine();
+            bw.write("  <xliff version=\"1.1\">");
+            bw.newLine();
+            bw.write("   <file");
+            bw.newLine();
+            bw.write("      source-language=\"en\"");
+            bw.newLine();
+            bw.write("      target-language=\"" + lang + "\">");
+            bw.newLine();
+            bw.write("      <body>");
+            bw.newLine();
+            for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+                echoAsCSV(wb.getSheetAt(i), bw, lang);
+            }
+            bw.write("      </body>");
+            bw.newLine();
+            bw.write("    </file>");
+            bw.newLine();
+            bw.write("  </xliff>");
+            bw.close();
+        } catch (Exception ex) {
+            Logger.getLogger(ExcelReading.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                inp.close();
+            } catch (IOException ex) {
+                Logger.getLogger(ExcelReading.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private static String getLocale(String fileName) {
+
+        String localePatternStr = "/[a-zA-Z]{2,2}+_+[a-zA-Z]{2,2}/";
+
+        // Compile Regex expression
+        Pattern localePattern = Pattern.compile(localePatternStr);
+
+        // Match locale+directory pattern in original URL
+        Matcher localeMatcher = localePattern.matcher(fileName);
+        String urlLocal ="en-gb";
+        if (localeMatcher.find()) {
+            urlLocal  = localeMatcher.group(1);
+        }
+
+        return urlLocal;
+    }
+}
